2f1bc30812a9eefcbb84fe31b5bf2610147b827d
P666R <sam469roger@gmail.com>
2025-02-04 14:36:54 +0530

    fix: Prettier CLI cmds

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { ThemeProvider, CssBaseline } from '@mui/material';
 import { customTheme } from './theme/customTheme';
 
 const App: FC = (): ReactElement => {
-    return (
-        <ThemeProvider theme={customTheme}>
-            <CssBaseline />
-            App
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      App
+    </ThemeProvider>
+  );
 };
 
 export default App;
