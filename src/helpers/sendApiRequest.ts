724fd0d11e0df4f8699a61f32eefb85512000c9c
P666R <sam469roger@gmail.com>
2025-02-11 07:38:52 +0530

    feat: Setup fetch api

---
+++ src/helpers/sendApiRequest.ts
@@ -0,0 +1,30 @@
+type Method = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+
+function returnCorrectRequest(
+  method: Method,
+  data: unknown,
+): RequestInit {
+  const headers = { 'Content-Type': 'application/json' };
+  return method === 'GET'
+    ? { method, headers }
+    : { method, headers, body: JSON.stringify(data) };
+}
+
+export async function sendAPiRequest<T>(
+  url: string,
+  method: Method,
+  data: unknown = {},
+): Promise<T> {
+  const response = await fetch(
+    url,
+    returnCorrectRequest(method, data),
+  );
+
+  if (!response.ok) {
+    throw new Error(
+      `An error has occurred: ${response.status}`,
+    );
+  }
+
+  return response.json() as Promise<T>;
+}
